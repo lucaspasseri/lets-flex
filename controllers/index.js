0ca69ff0f6e206f600b2c0eeccd0823011f401c1
Lucas Passeri <lucaspasseri@poli.ufrj.br>
2026-05-09 00:36:58 -0300

    Add a layout and styles to dashboard page (wip)

--- controllers/index.js
+++ controllers/index.js
@@ -1,5 +1,4 @@
-import * as usersDb from "../db/users/index.js";
-import * as programsDb from "../db/programs/index.js";
+// import * as programsDb from "../db/programs/index.js";
 // import * as goalsDb from "../db/goals/index.js";
 // import * as cyclesDb from "../db/cycles/index.js";
 // import * as sessionsDb from "../db/sessions/index.js";
@@ -13,7 +12,6 @@ import * as programsDb from "../db/programs/index.js";
 // import * as exerciseVariantsDb from "../db/exercise_variants/index.js";
 
 async function getIndex(_req, res) {
-	const userArr = await usersDb.getAllUsers();
 	// const goalArr = await goalsDb.getAllGoals();
 	// const programArrWithoutIds = await programsDb.getAllProgramsWithoutIds();
 	// const cycleArr = await cyclesDb.getAllCyclesWithoutIds();
@@ -42,7 +40,7 @@ async function getIndex(_req, res) {
 
 	res.render("index", {
 		title: "Let's Flex!",
-		userArr,
+
 		// currUserPrograms,
 	});
 }
--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,18 @@ import { Router } from "express";
 import { getIndex } from "../controllers/index.js";
 import { getCurrentUser } from "../middlewares/getCurrentUser.js";
 import { getUrlAndPath } from "../middlewares/getUrlAndPath.js";
+import { getCurrentProgram } from "../middlewares/getCurrentProgram.js";
+import { getHelpers } from "../middlewares/getHelpers.js";
 
 const router = Router();
 
-router.get("/", getUrlAndPath, getCurrentUser, getIndex);
+router.get(
+	"/",
+	getUrlAndPath,
+	getHelpers,
+	getCurrentUser,
+	getCurrentProgram,
+	getIndex,
+);
 
 export default router;
