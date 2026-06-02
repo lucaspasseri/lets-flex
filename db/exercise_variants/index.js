3f8e1990e0b83ec21dc7e3b229e5ced1838bf3a3
Lucas Passeri <lucaspasseri@poli.ufrj.br>
2026-06-02 16:08:58 -0300

    Add new library page layout

--- db/exercise_variants/index.js
+++ db/exercise_variants/index.js
@@ -2,7 +2,22 @@ import pool from "../pool.js";
 import toNullableNumber from "../../utils/toNullableNumber.js";
 
 async function getAllExerciseVariants() {
-	const { rows } = await pool.query("SELECT * FROM exercise_variants");
+	const { rows } = await pool.query(
+		`
+	SELECT
+		exercise_variants.*,
+		equipments.name AS equipment_name,
+		exercises.name AS exercise_name,
+		movement_patterns.name AS movement_pattern_name
+	FROM exercise_variants
+	LEFT JOIN equipments
+		ON exercise_variants.equipment_id = equipments.id
+	LEFT JOIN exercises
+		ON exercise_variants.exercise_id = exercises.id
+	LEFT JOIN movement_patterns
+		ON exercises.movement_pattern_id = movement_patterns.id;
+		`,
+	);
 	return rows;
 }
 
