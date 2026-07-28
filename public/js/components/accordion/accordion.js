c9a101d12d90f2f7a9be48972cbffae62af15b93
Lucas Passeri <lucaspasseri@poli.ufrj.br>
2026-07-28 12:35:23 -0300

    Refactor accordion component (wip)

--- public/js/components/accordion/accordion.js
+++ public/js/components/accordion/accordion.js
@@ -1,10 +1,16 @@
 function createAccordion(root) {
-	const trigger = root.querySelector("[data-accordion-heading]");
+	const trigger = root.querySelector("[data-accordion-header]");
 	const panel = root.querySelector("[data-accordion-panel]");
 
-	trigger.addEventListener("click", () => {
-		collapsedClassToggle(root, panel);
-	});
+	trigger.addEventListener("click", () => collapsedClassToggle(root, panel));
+	trigger.addEventListener("keydown", e => handleKeyDown(e, root, panel));
+
+	if (panel.hidden) {
+		trigger.classList.add("collapsed");
+		trigger.setAttribute("aria-expanded", false);
+	} else {
+		trigger.setAttribute("aria-expanded", true);
+	}
 }
 
 function collapsedClassToggle(accordion, panel) {
@@ -13,13 +19,27 @@ function collapsedClassToggle(accordion, panel) {
 
 		setTimeout(() => {
 			panel.hidden = !panel.hidden;
-		}, 220);
+			accordion
+				.querySelector("[data-accordion-header]")
+				.setAttribute("aria-expanded", !panel.hidden);
+		}, 200);
 	} else {
 		panel.hidden = !panel.hidden;
+		accordion
+			.querySelector("[data-accordion-header]")
+			.setAttribute("aria-expanded", !panel.hidden);
 
 		setTimeout(() => {
 			accordion.classList.toggle("collapsed");
-		}, 30);
+		}, 20);
+	}
+}
+
+function handleKeyDown(e, root, panel) {
+	const key = e.code;
+
+	if (key === "Space" || key === "Enter") {
+		collapsedClassToggle(root, panel);
 	}
 }
 
